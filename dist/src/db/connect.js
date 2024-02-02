45d488772baf8254a8a1c9810a34a34b020dcdfa
chosen2730 <cf.youvarsity@gmail.com>
2024-02-02 22:48:40 +0100

    removed dist from repo

--- dist/src/db/connect.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConnectDB = void 0;
-const mongoose = require("mongoose");
-const ConnectDB = (url) => {
-    return mongoose.connect(url);
-};
-exports.ConnectDB = ConnectDB;
